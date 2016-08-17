798761fa0c59a4e8cf423d563b0dbd426f592ece
unknown <ivanp@dump.hr>
2016-08-17 21:05:58 +0200

    finished continue sequence task flow

--- AuThink.Desktop/ViewModels/EndTestViewModel.cs
+++ AuThink.Desktop/ViewModels/EndTestViewModel.cs
@@ -3,10 +3,123 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuThink.Desktop.Core;
+using AuThink.Desktop.Services;
+using AuThink.Desktop.Settings;
+using AuThink.Desktop.Views;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace AuThink.Desktop.ViewModels
 {
-    public class EndTestViewModel
+    public partial class EndTestViewModel : ViewModelBase
     {
+        public string SuccessfullTextContent
+        {
+            get { return _successfullTextContent; }
+            set
+            {
+                if (_successfullTextContent == value)
+                {
+                    return;
+                }
+
+                _successfullTextContent = value;
+                this.RaisePropertyChanged(this.SuccessfullTextContent);
+            }
+        }
+        private string _successfullTextContent = Language.EndTestPage.SuccessfullTextContent();
+
+        public string ResetTestButtonContent
+        {
+            get { return _resetTestButtonContent; }
+            set
+            {
+                if (_resetTestButtonContent == value)
+                {
+                    return;
+                }
+
+                _resetTestButtonContent = value;
+                this.RaisePropertyChanged(this.ResetTestButtonContent);
+            }
+        }
+        private string _resetTestButtonContent = Language.EndTestPage.ResetTestButtonContent();
+
+        public string TestMenuButtonContent
+        {
+            get { return _testMenuButtonContent; }
+            set
+            {
+                if (_testMenuButtonContent == value)
+                {
+                    return;
+                }
+
+                _testMenuButtonContent = value;
+                this.RaisePropertyChanged(this.TestMenuButtonContent);
+            }
+        }
+        private string _testMenuButtonContent = Language.EndTestPage.TestMenuButtonContent();
+
+        public string ExitButtonContent
+        {
+            get { return _exitButtonContent; }
+            set
+            {
+                if (_exitButtonContent == value)
+                {
+                    return;
+                }
+
+                _exitButtonContent = value;
+                this.RaisePropertyChanged(this.ExitButtonContent);
+            }
+        }
+        private string _exitButtonContent = Language.EndTestPage.ExitButtonContent();
+
+        public RelayCommand ResetTestCommand { get; set; }
+        public void ResetTest()
+        {
+            var taskIds = taskQueries.GetAllTasksForTest(GameState.CurrentTestId).Select(task => task.Id).ToList();
+            GameState.Start(GameState.CurrentTestId, taskIds);
+            var view = new GameView();
+            navigationService.NavigateTo(view);
+        }
+
+        public RelayCommand GoToTestListCommand { get; set; }
+        public void GoToTestList()
+        {
+            var view = new TestListView();
+            navigationService.NavigateTo(view);
+        }
+
+        public RelayCommand ExitToMainCommand { get; set; }
+        public void ExitToMain()
+        {
+            var view = new MainMenu();
+            navigationService.NavigateTo(view);
+        }
+
+    }
+
+    public partial class EndTestViewModel
+    {
+        public EndTestViewModel
+        (
+            AuthinkNavigationService navigationService,
+            ITaskQueries taskQueries
+        )
+        {
+            this.ResetTestCommand = new RelayCommand(ResetTest);
+            this.GoToTestListCommand = new RelayCommand(GoToTestList);
+            this.ExitToMainCommand = new RelayCommand(ExitToMain);
+
+            this.navigationService = navigationService;
+            this.taskQueries = taskQueries;
+        }
+
+        private readonly AuthinkNavigationService navigationService;
+        private readonly ITaskQueries taskQueries;
     }
 }
--- AuThink.Desktop/ViewModels/RewardViewModel.cs
+++ AuThink.Desktop/ViewModels/RewardViewModel.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AuThink.Desktop.Services;
+using AuThink.Desktop.Settings;
+using AuThink.Desktop.Views;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace AuThink.Desktop.ViewModels
 {
-    public class RewardViewModel
+    public partial class RewardViewModel : ViewModelBase
     {
+        public RelayCommand TempRewordCommand { get; private set; }
+        public void Continue()
+        {
+            GameState.EndTask();
+            //System.Threading.Tasks.Task.Delay(2000);
+            if (GameState.TaskIds.Any())
+            {
+                var view = new GameView();
+                _navigationService.NavigateTo(view);
+            }
+
+            else
+            {
+                var view = new EndTestView();
+                _navigationService.NavigateTo(view);
+            }
+        }
+
+        public string ContinueButtonContent
+        {
+            get { return _continueButtonContent; }
+            set
+            {
+                if (_continueButtonContent == value)
+                {
+                    return;
+                }
+
+                _continueButtonContent = value;
+                this.RaisePropertyChanged(this.ContinueButtonContent);
+            }
+        }
+        private string _continueButtonContent = Language.RewardPage.ContinueButtonContent();
+
+        public string RewardTextContent
+        {
+            get { return _rewardTextContent; }
+            set
+            {
+                if (_rewardTextContent == value)
+                {
+                    return;
+                }
+
+                _rewardTextContent = value;
+                this.RaisePropertyChanged(this.RewardTextContent);
+            }
+        }
+        private string _rewardTextContent = Language.RewardPage.RewardTextContent();
+    }
+
+    public partial class RewardViewModel
+    {
+        private readonly AuthinkNavigationService _navigationService;
+        public Uri SoundUrl { get; set; }
+
+        public RewardViewModel(AuthinkNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            this.TempRewordCommand = new RelayCommand(Continue);
+            //this.SoundUrl = (bool)ApplicationData.Current.LocalSettings.Values["IsRewardSoundEnabled"] ? new Uri("ms-appx:///Resources/Sounds/aplauz-dugi.mp3") : null;
+        }
     }
 }
